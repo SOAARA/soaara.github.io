d598ecda95c09a6209f4888f068b0267b2c3d808
godmoonl <himoon345@gmail.com>
2021-10-28 01:07:47 +0900

    aa

---
+++ src/Components/router.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {HashRouter as Router} from "react-router-dom"
+
+const AppRouter=()=>{
+  return(
+    <Router>
+        
+    </Router>
+  )
+}
+
+export default AppRouter
