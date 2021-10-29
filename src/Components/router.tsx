ad5f9bf41af7fc0fe8fb9bedb7fe2817e45543aa
godmoonl <himoon345@gmail.com>
2021-10-29 16:49:09 +0900

    fullpage test

--- src/Components/router.tsx
+++ src/Components/router.tsx
@@ -3,20 +3,23 @@ import {HashRouter as Router,Route, Switch} from "react-router-dom"
 import About from "../routes/About"
 import Members from "../routes/Members"
 import Navigation from "./Navigation";
+import Page from "./Home";
 
 const AppRouter=()=>{
   return(
     <Router>
       <Navigation/>
-      <Switch>
-        
-        <Route exact path="/about">
-          <About/>
-        </Route>
-        <Route exact path="/members">
-          <Members/>
-        </Route>
-      </Switch>
+        <Switch>
+          <Route exact path="/">
+            <Page/>
+          </Route>
+          <Route exact path="/about">
+            <About/>
+          </Route>
+          <Route exact path="/members">
+            <Members/>
+          </Route>
+        </Switch>
     </Router>
   )
 }
